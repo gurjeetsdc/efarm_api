f86f2c68552c64824758019359583130bb2a6dd5
Gurjeet Singh <gurjeets@smartdatainc.net>
2017-04-03 18:16:56 +0530

    Updated swagger Documentation

--- src/routes/crop.js
+++ src/routes/crop.js
@@ -4,11 +4,139 @@ var router  = express.Router();
 var authController   = require('../controllers/auth');
 var cropController   = require('../controllers/crop');
 
-router.get('/crops',authController.isAuthenticated, cropController.getCrops);
+/**
+ * @swagger
+ * 	definition:
+ *   crop:
+ *     properties:
+ *       name:
+ *         type: string
+ *       description:
+ *         type: string
+ *       price:
+ *         type: integer
+ *       quantity:
+ *         type: integer
+ */
+
+
+/**
+* @swagger
+* /crops:
+*   get:
+*     description: get the list of all the crops added by user.
+*     tags: [Crops]
+*     responses:
+*       200:
+*         description: API is working.
+*         schema:
+*           type: object
+*/
+/**
+* @swagger
+* /crops:
+*   post:
+*     description: Creates a new crop
+*     tags: [Crops]
+*     produces:
+*       - application/json
+*     parameters:
+*       - name: name
+*         description: crop name
+*         in: formData
+*         required: true
+*         type: string
+
+*       - name: description
+*         description: crop description
+*         in: formData
+*         required: true
+*         type: string
+
+*       - name: price
+*         description: crop price
+*         in: formData
+*         required: true
+*         type: integer
+
+*       - name: quantity
+*         description: crop quantity
+*         in: formData
+*         required: true
+*         type: integer
+*     responses:
+*       200:
+*         description: successfully created
+*         schema:
+*           type: object
+*           $ref: '#/definitions/crop'
+*/
 router.post('/crops',authController.isAuthenticated, cropController.postCrops);
 
+/**
+ * @swagger
+ * /crops/{crop_id}:
+ *   get:
+ *     tags:
+ *       - Crops
+ *     description: Returns a single crop
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: crop_id
+ *         description: crop's id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: A single Crop
+ *         schema:
+ *           $ref: '#/definitions/crop'
+ */
+
 router.get('/crops/:crop_id', authController.isAuthenticated,cropController.getCrop);
+
+/**
+ * @swagger
+ * /crops/{id}:
+ *   put:
+ *     tags: Crops
+ *     description: Updates a single Crop
+ *     produces: application/json
+ *     parameters:
+ *       name: crop
+ *       in: body
+ *       description: Fields for the crop resource
+ *       schema:
+ *         type: array
+ *         $ref: '#/definitions/crop'
+ *     responses:
+ *       200:
+ *         description: Successfully updated
+ */
+
 router.put('/crops/:crop_id', authController.isAuthenticated, cropController.putCrop);
+
+/**
+ * @swagger
+ * /crops/{crop_id}:
+ *   delete:
+ *     tags:
+ *       - Crops
+ *     description: Deletes a single crop
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: crop_id
+ *         description: crops's id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: Successfully deleted
+ */
 router.delete('/crops/:crop_id',authController.isAuthenticated, cropController.deleteCrop);
 
 module.exports = router;
